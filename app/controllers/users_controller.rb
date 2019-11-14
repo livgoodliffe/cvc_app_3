0ab4428b6669e8e0852ee6c0805848a37d3acfb4
Olivia Goodliffe <ocgoodliffe@gmail.com>
2019-11-14 15:09:48 +1100

    add name and company to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,10 @@ class UsersController < ApplicationController
   end
 
   def show
+    @current_user = current_user
+    @params = params
+    @user = User.find(params[:id])
+    @projects = Project.where(user_id: @user)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_12_061839) do
+ActiveRecord::Schema.define(version: 2019_11_14_040911) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 2019_11_12_061839) do
     t.integer "score"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
   create_table "soft_elements", force: :cascade do |t|
@@ -45,8 +47,11 @@ ActiveRecord::Schema.define(version: 2019_11_12_061839) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.string "company"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "projects", "users"
 end
